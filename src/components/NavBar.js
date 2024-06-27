65bc29dc7bd52395d714acbff93f4b6891b470be
Amos Augusto <amosaugustos@gmail.com>
2024-06-27 17:02:28 +0700

    add navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon2 from '../assets/img/github-mark-white.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
-function NavBar() {
+export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
 
@@ -46,9 +46,9 @@ function NavBar() {
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="#"><img src={navIcon1} alt="" /></a>
-                            <a href="#"><img src={navIcon2} alt="" /></a>
-                            <a href="#"><img src={navIcon3} alt="" /></a>
+                            <a href="https://www.linkedin.com/in/amos-augusto-silangit/"><img src={navIcon1} alt="" /></a>
+                            <a href="https://github.com/amosaugusto"><img src={navIcon2} alt="" /></a>
+                            <a href="https://www.instagram.com/amosilangit/"><img src={navIcon3} alt="" /></a>
                         </div>
                         <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
                     </span>
@@ -57,5 +57,3 @@ function NavBar() {
         </Navbar>
     );
 }
-
-export default NavBar;
